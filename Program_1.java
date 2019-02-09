65f9a7847c29615e491b2d3307a61c5d78a7bc3b
sonicaks <adityakumarsinha2014@gmail.com>
2019-02-10 02:20:12 +0530

    Solve question 1

---
+++ Program_1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+
+public class Program_1 {
+    public static void main(String args[]) {
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            System.out.println("IP Address: " + address.getHostAddress());
+            System.out.println("Host Name: " + address.getHostName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
