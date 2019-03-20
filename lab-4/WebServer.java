7852bda1b20623c2e7be187ef35e668180740537
sonicaks <adityakumarsinha2014@gmail.com>
2019-03-20 11:13:26 +0530

    Implement web server capable of handling http GET request

--- lab-4/WebServer.java
+++ lab-4/WebServer.java
@@ -1,14 +1,16 @@
 import java.io.*;
+import java.util.*;
+import java.net.*;
+import com.sun.net.httpserver.*;
 
 class WebServer {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         /* Create HttpServer */
         int port = 8000;
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         System.out.println("Server started at " + port);
         server.createContext("/", new RootHandler());
-        server.createContext("/echoGet", new EchoGetHandler());
-        server.createContext("/echoPost", new EchoPostHandler());
+        server.createContext("/Get", new GetHandler());
         server.setExecutor(null);
         server.start();
     }
@@ -16,12 +18,66 @@ class WebServer {
 
 class RootHandler implements HttpHandler {
     public void handle(HttpExchange httpExch) throws IOException {
-        String resp = "<h4>Server started successfully at port: </h4>" + "<h4>" + port + "</h4>";
-        httpExch.sendResponseHeader(200, resp.length());
+        int port = 8000;
+        String resp = "Server started successfully at port: " + port;
+        httpExch.sendResponseHeaders(200, resp.length());
         OutputStream os = httpExch.getResponseBody();
         os.write(resp.getBytes());
         os.close();
     }
 }
 
-/* https://www.codeproject.com/tips/1040097/create-a-simple-web-server-in-java-http-server */
+class GetHandler implements HttpHandler {
+    public void handle(HttpExchange httpExch) throws IOException {
+        /* Parse request */
+        Map<String, Object> params = new HashMap<String, Object>();
+        URI reqUri = httpExch.getRequestURI();
+        String qry = reqUri.getRawQuery();
+        ParseQry.parseQry(qry, params);
+
+        /* Send response */
+        String resp = "";
+        for (String key : params.keySet()) {
+            resp += key + " = " + params.get(key) + "\n";
+        }
+        httpExch.sendResponseHeaders(200, resp.length());
+        OutputStream os = httpExch.getResponseBody();
+        os.write(resp.toString().getBytes());
+        os.close();
+    }
+}
+
+class ParseQry {
+    public static void parseQry(String qry, Map<String, Object> params) throws UnsupportedEncodingException {
+        if (qry != null) {
+            String pairs[] = qry.split("[&]");
+            for (String pair : pairs) {
+                String paramsStr[] = pair.split("[=]");
+                String key = null;
+                String val = null;
+                if (paramsStr.length > 0) {
+                    key = URLDecoder.decode(paramsStr[0], System.getProperty("file.encoding"));
+                }
+
+                if (paramsStr.length > 1) {
+                    val = URLDecoder.decode(paramsStr[1], System.getProperty("file.encoding"));
+                }
+
+                if (params.containsKey(key)) {
+                    Object obj = params.get(key);
+                    if (obj instanceof List<?>) {
+                        List<String> vals = (List<String>) obj;
+                        vals.add(val);
+                    } else if (obj instanceof String) {
+                        List<String> vals = new ArrayList<String>();
+                        vals.add((String) obj);
+                        vals.add(val);
+                        params.put(key, vals);
+                    }
+                } else {
+                    params.put(key, val);
+                }
+            }
+        }
+    }
+}
